5b855eceb4ba782399644dd6787601c311f9fd53
Jesse Weaver <jeweaver@pivotal.io>
2018-09-12 17:48:22 -0600

    Notify user in title that their table is ready

--- frontend/src/Notifier.js
+++ frontend/src/Notifier.js
@@ -4,8 +4,28 @@ import ReactNotifications from 'react-browser-notifications';
 import pongIconBlackSrc from './resource-icon-black.svg';
 
 export default class Notifier extends Component {
+  componentDidMount() {
+    document.addEventListener('visibilitychange', this.onVisibilityChange);
+  }
+
+  onVisibilityChange = () => {
+    if (!document.hidden) {
+      this.setTitle(false);
+    }
+  }
+
+  setTitle(addNotification) {
+    if (addNotification) {
+      document.title = 'Pong - table is ready!'
+    } else {
+      document.title = 'Pong'
+    }
+  }
+
   show() {
     if (this.notifications && this.notifications.supported()) this.notifications.show();
+
+    this.setTitle(true);
   }
 
   constructor() {
@@ -21,8 +41,8 @@ export default class Notifier extends Component {
 
   render() {
     return <ReactNotifications
-      onRef={ref => (this.notifications = ref)} // Required
-      title="Your table is ready!" // Required
+      onRef={ref => (this.notifications = ref)}
+      title="Your table is ready!"
       body="Go grab it before it runs away!"
       icon={pongIconBlackSrc}
       onClick={this.onClick}
